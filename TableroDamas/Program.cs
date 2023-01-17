a743cf77b48c91e6b275895ea447ce15ee9625a3
yefferson Cardona <yeffercardona@hotmail.com>
2023-01-17 18:04:13 -0500

    tablero Damas

---
+++ TableroDamas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TableroDamas
+{
+    class Program
+    {
+		static int n = 5;
+		public static void Main(string[] args)
+        {
+			
+
+				if (n > 10 || n <= 0)
+				{
+					n = 5;
+				}
+				int a = n;
+				int b = n;
+				int[,] arrayTablero = new int[a,b];
+
+			for (int i = 0; i < arrayTablero.GetLength(0); i++)
+			{
+				bool x = i % 2 == 0;
+
+
+
+				for (int j = 0; j < arrayTablero.GetLength(1); j++)
+				{
+					Console.Write(x ? "X" : "_");
+					x = !x;
+				}
+				Console.WriteLine("");
+			
+		}
+	}
+    }
+}
